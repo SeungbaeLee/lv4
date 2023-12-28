08f588aca7623c326963f7014ca4006fb72f30b7
SeungbaeLee <aha224610@gmail.com>
2023-12-28 23:03:53 +0900

    init

---
+++ src/main/java/com/hh99/lv4/Lv4Application.java
@@ -0,0 +1,13 @@
+package com.hh99.lv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lv4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hh99/lv4/Lv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh99.lv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
