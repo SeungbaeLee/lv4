2d522ef16a82413afe10e281e1604945297b77e8
SeungbaeLee <aha224610@gmail.com>
2023-12-28 23:13:55 +0900

    feat : memberDto 구현 / toEntity,fromEntity

--- src/main/java/com/hh99/lv4/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/hh99/lv4/domain/member/dto/MemberResponseDto.java
@@ -1,4 +1,40 @@
 package com.hh99.lv4.domain.member.dto;
 
+import com.hh99.lv4.domain.member.entity.Member;
+import com.hh99.lv4.domain.member.entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class MemberResponseDto {
+
+    private Long memberId;
+    private String email;
+    private String gender;
+    private String phoneNumber;
+    private String address;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public MemberResponseDto(Long memberId, String email, String gender, String phoneNumber, String address, Role role) {
+        this.memberId = memberId;
+        this.email = email;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+
+    public static MemberResponseDto fromEntity(Member member) {
+        return MemberResponseDto.builder()
+                .email(member.getEmail())
+                .gender(member.getGender())
+                .phoneNumber(member.getPhoneNumber())
+                .address(member.getAddress())
+                .role(member.getRole())
+                .build();
+    }
 }
