a4350656412d01ffd9869b4084c1a319d58aaac1
SeungbaeLee <aha224610@gmail.com>
2023-12-28 23:26:50 +0900

    feat : Instructor Dto 구현 / toEntity, fromEntity

--- src/main/java/com/hh99/lv4/domain/instructor/dto/InstructorPostDto.java
+++ src/main/java/com/hh99/lv4/domain/instructor/dto/InstructorPostDto.java
@@ -1,4 +1,31 @@
 package com.hh99.lv4.domain.instructor.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Getter
+@NoArgsConstructor
 public class InstructorPostDto {
+
+    @NonNull
+    public String instructorName;
+    @NonNull
+    public long years;
+    @NonNull
+    public String company;
+    @NonNull
+    public String phoneNumber;
+    @NonNull
+    public String instruction;
+
+    @Builder
+    public InstructorPostDto(String instructorName, long years, String company, String phoneNumber, String instruction) {
+        this.instructorName = instructorName;
+        this.years = years;
+        this.company = company;
+        this.phoneNumber = phoneNumber;
+        this.instruction = instruction;
+    }
 }
--- src/main/java/com/hh99/lv4/domain/instructor/dto/InstructorResponseDto.java
+++ src/main/java/com/hh99/lv4/domain/instructor/dto/InstructorResponseDto.java
@@ -1,4 +1,36 @@
 package com.hh99.lv4.domain.instructor.dto;
 
+import com.hh99.lv4.domain.instructor.entity.Instructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class InstructorResponseDto {
+    public long instructorId;
+    public String instructorName;
+    public long years;
+    public String company;
+    public String phoneNumber;
+    public String instruction;
+
+    @Builder
+    public InstructorResponseDto(long instructorId, String instructorName, long years, String company, String phoneNumber, String instruction) {
+        this.instructorId = instructorId;
+        this.instructorName = instructorName;
+        this.years = years;
+        this.company = company;
+        this.phoneNumber = phoneNumber;
+        this.instruction = instruction;
+    }
+
+    public static InstructorResponseDto fromEntity(Instructor instructor) {
+        return InstructorResponseDto.builder()
+                .instructorId(instructor.getInstructorId())
+                .instructorName(instructor.getInstructorName())
+                .years(instructor.getYears())
+                .company(instructor.getCompany())
+                .phoneNumber(instructor.getPhoneNumber())
+                .instruction(instructor.getIntroduction())
+                .build();
+    }
 }
