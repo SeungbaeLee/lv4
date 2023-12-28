54cd9f970d8d1b88e1fe05bf108f627325019065
SeungbaeLee <aha224610@gmail.com>
2023-12-28 23:33:23 +0900

    feat : 유효성 검사 추가

--- src/main/java/com/hh99/lv4/domain/member/controller/MemberController.java
+++ src/main/java/com/hh99/lv4/domain/member/controller/MemberController.java
@@ -4,10 +4,12 @@ import com.hh99.lv4.domain.member.dto.MemberPostDto;
 import com.hh99.lv4.domain.member.dto.MemberResponseDto;
 import com.hh99.lv4.domain.member.entity.Member;
 import com.hh99.lv4.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity createMember(MemberPostDto postDto) {
+    public ResponseEntity createMember(@Valid @RequestBody MemberPostDto postDto) {
         MemberResponseDto responseDto = memberService.createMember(postDto);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
