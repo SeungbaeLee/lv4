755dbb7fcac6c84b2fbebe8eb677bf8cc5ad4a87
SeungbaeLee <aha224610@gmail.com>
2023-12-28 23:23:18 +0900

    feat : createMember 구현(password hashing X)

--- src/main/java/com/hh99/lv4/domain/member/controller/MemberController.java
+++ src/main/java/com/hh99/lv4/domain/member/controller/MemberController.java
@@ -1,4 +1,26 @@
 package com.hh99.lv4.domain.member.controller;
 
+import com.hh99.lv4.domain.member.dto.MemberPostDto;
+import com.hh99.lv4.domain.member.dto.MemberResponseDto;
+import com.hh99.lv4.domain.member.entity.Member;
+import com.hh99.lv4.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity createMember(MemberPostDto postDto) {
+        MemberResponseDto responseDto = memberService.createMember(postDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/hh99/lv4/domain/member/service/MemberService.java
+++ src/main/java/com/hh99/lv4/domain/member/service/MemberService.java
@@ -1,4 +1,34 @@
 package com.hh99.lv4.domain.member.service;
 
+import com.hh99.lv4.domain.member.dto.MemberPostDto;
+import com.hh99.lv4.domain.member.dto.MemberResponseDto;
+import com.hh99.lv4.domain.member.entity.Member;
+import com.hh99.lv4.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponseDto createMember(MemberPostDto postDto) {
+
+//        String encryptedPassword = passwordEncoder.encode(postDto.getPassword());
+
+        Member member = Member.builder()
+                .email(postDto.getEmail())
+                .password(postDto.getPassword())
+                .gender(postDto.getGender())
+                .phoneNumber(postDto.getPhoneNumber())
+                .address(postDto.getAddress())
+                .role(postDto.getRole())
+                .build();
+
+        Member savedMember = memberRepository.save(member);
+        return MemberResponseDto.fromEntity(savedMember);
+    }
 }
