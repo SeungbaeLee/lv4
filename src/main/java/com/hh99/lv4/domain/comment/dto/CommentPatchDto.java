6837572a3fb65bcd4576a2081f5f1e60aa028f01
SeungbaeLee <aha224610@gmail.com>
2023-12-29 17:05:32 +0900

    feat : comment dto 작성

--- src/main/java/com/hh99/lv4/domain/comment/dto/CommentPatchDto.java
+++ src/main/java/com/hh99/lv4/domain/comment/dto/CommentPatchDto.java
@@ -5,8 +5,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CommentPostDto {
+public class CommentPatchDto {
 
     private String content;
-
 }
--- src/main/java/com/hh99/lv4/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/hh99/lv4/domain/comment/dto/CommentResponseDto.java
@@ -1,4 +1,26 @@
 package com.hh99.lv4.domain.comment.dto;
 
+import com.hh99.lv4.domain.comment.entity.Comment;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class CommentResponseDto {
+
+    private Long commentId;
+
+    private String content;
+
+    @Builder
+    public CommentResponseDto(Long commentId, String content) {
+        this.commentId = commentId;
+        this.content = content;
+    }
+
+    public static CommentResponseDto fromEntity(Comment comment) {
+        return CommentResponseDto.builder()
+                .commentId(comment.getCommentId())
+                .content(comment.getContent())
+                .build();
+    }
 }
